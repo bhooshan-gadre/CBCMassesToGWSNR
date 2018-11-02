9d41a7b48e5c64a89ea384a4824d5682eb78197a
Parth92 <jadhav.shreejit@gmail.com>
2018-11-02 15:16:18 +0530

    updated space limits based on template based SNR

--- src/Limits_on_visible_space.py
+++ src/Limits_on_visible_space.py
@@ -1,18 +1,17 @@
 from function_SNR import *
 import matplotlib.pyplot as plt
 
-# Defining numpy array of r
-r = np.arange(1., 5000., 0.125)
-r *= Mpc
+# Taking r = 100 Mpc
+r = np.array([100.*Mpc])
 r = {'S6':r, 'O1':r, 'O2':r, 'Design':r}
 # limiting masses
-mass_lim = {'Lower Cutoff': 50., 'Upper Cutoff': 5.}
+mass_lim = {'Lower Space Cutoff': 50., 'Upper Space Cutoff': 5.}
 # SNR cutoffs
-cut = {'Lower Cutoff': 60., 'Upper Cutoff': 5.}
+cut = {'Lower Space Cutoff': 60., 'Upper Space Cutoff': 5.}
 # dict of SNRs which will contain SNR dicts against cutoffs. These SNR dicts in turn contain SNR arrays corresponding to diff RUNs
-SNRs = {'Lower Cutoff': {}, 'Upper Cutoff': {}}
+SNRs = {'Lower Space Cutoff': {}, 'Upper Space Cutoff': {}}
 # limits on space
-r_extreme = {'Lower Cutoff': {}, 'Upper Cutoff': {}}
+r_extreme = {'Lower Space Cutoff': {}, 'Upper Space Cutoff': {}}
 
 # x_l = 0.
 # x_u = 3000.
@@ -24,33 +23,42 @@ r_extreme = {'Lower Cutoff': {}, 'Upper Cutoff': {}}
 
 for cutoff in cut.keys():
 	# creating injection arrays with cutoff mass pairs
-	M1 = np.ones(len(r['S6'])) * mass_lim[cutoff] * Msun
-	M2 = np.ones(len(r['S6'])) * mass_lim[cutoff] * Msun
+	M1 = np.array([mass_lim[cutoff]]) * Msun
+	M2 = np.array([mass_lim[cutoff]]) * Msun
 	# finding SNRs for respective cutoffs for diff RUNs
-	SNRs[cutoff]  = find_simple_SNR(M1, M2, r)
+	SNRs[cutoff]  = find_pycbc_SNR(M1, M2, r, [0.], [0.], [0.])
 	# find extremity in space corresponding to the SNR cutoff
 	for RUN in SNRs[cutoff].keys():
-		r_extreme[cutoff][RUN] = r[RUN][np.where(min((SNRs[cutoff][RUN] - cut[cutoff])**2.) == ((SNRs[cutoff][RUN] - cut[cutoff])**2.))]
+		r_extreme[cutoff][RUN] = SNRs[cutoff][RUN] * r[RUN] / cut[cutoff] / Mpc
 
 print r_extreme
 #########################################
 # 				Output:
 
-# {'Lower Cutoff': {'Design': array([3.25847834e+25]), 'S6': array([5.01423041e+22]), 'O1': array([5.52336765e+24]), 'O2': array([8.26576597e+24])}, 'Upper Cutoff': {'Design': array([1.06521539e+26]), 'S6': array([5.21479962e+24]), 'O1': array([2.97729573e+25]), 'O2': array([3.77802976e+25])}}
+# With find_pycbc_SNR
+# {'Upper Space Cutoff':
+# {'Design': array([3279.12309453]),
+# 'S6': array([153.40926056]),
+# 'O1': array([901.50600027]),
+# 'O2': array([1153.03794345])},
+# 'Lower Space Cutoff':
+# {'Design': array([1587.82130486]),
+# 'S6': array([82.77513475]),
+# 'O1': array([454.18511569]),
+# 'O2': array([576.59669512])}}
 
-# OR
 
-# {'Lower Cutoff': 
-# {'Design': array([ 1056.]),
-#   'O1': array([ 179.]),
-#   'O2': array([ 267.875]),
-#   'S6': array([ 1.625])},
-
-# 'Upper Cutoff': 
-# {'Design': array([ 3452.125]),
-#   'O1': array([ 964.875]),
-#   'O2': array([ 1224.375]),
-#   'S6': array([ 169.])}}
+# With find_simple_SNR
+# {'Upper Space Cutoff': 
+# {'Design': array([3452.14263401]),
+#  'O1': array([964.90328158]),
+#  'O2': array([1224.40665554]),
+#  'S6': array([169.03614154])},
+# 'Lower Space Cutoff': 
+# {'Design': array([1056.05949735]),
+#  'O1': array([179.04932493]),
+#  'O2': array([267.89876495]),
+#  'S6': array([1.56752347])}
 #########################################
 
 # Plotting
